1ce7ce4b25fc52b99dd63d2a9a75334e42d035f9
Grzegorz Woźniak <grzegorz.wozniak1989@gmail.com>
2023-02-27 18:46:14 +0100

    Initial commit

---
+++ src/orders/orders.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Param, Delete } from '@nestjs/common';
+import { OrdersService } from './orders.service';
+
+@Controller('orders')
+export class OrdersController {
+  constructor(private orderService: OrdersService) {}
+
+  @Get('/')
+  getAll(): any {
+    return this.orderService.getAll();
+  }
+
+  @Get('/:id')
+  getById(@Param('id') id: string) {
+    return this.orderService.getById(id);
+  } 
+}
